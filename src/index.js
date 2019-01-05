a79dff7de54c6bc08e93a66a9020f907ea20c797
Seth Wilson <sethhwilson@gmail.com>
2019-01-04 20:47:51 -0600

    Changed title header.

--- src/index.js
+++ src/index.js
@@ -147,7 +147,7 @@ class App extends Component {
   render() {
     return (
       <div className="container-fluid p-5">
-        <h1>CodeSpeak.io</h1>
+        <h1>CodeSpeak.io - Voice Programming on the Web</h1>
         <div className="row">
           <div className="col-md-2">
             <div className="field">
